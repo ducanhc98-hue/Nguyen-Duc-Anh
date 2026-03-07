a3e9c73c83212c891e3152815c5910a63b623156
ducanhc98-hue <ducanhc98@gmail.com>
2026-03-07 23:37:39 +0700

    delete

---
+++ mang giam dan gia tri.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Nhap mang:";
+    cin>>n;
+    int a[100];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            if(a[i]<a[j]){
+                int x= a[i];
+                a[i]= a[j];
+                a[j]= x;
+            }
+        }
+    }
+    for(int i=0; i<n; i++){
+        cout<<a[i]<<"";
+    }
+    return 0;
+}
