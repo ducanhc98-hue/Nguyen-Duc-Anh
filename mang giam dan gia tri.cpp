16ca13c2699aaaae568bb2f4087ffc28cae5deac
ducanhc98-hue <ducanhc98@gmail.com>
2026-03-10 12:11:57 +0700

    fix

--- mang giam dan gia tri.cpp
+++ mang giam dan gia tri.cpp
@@ -3,23 +3,24 @@ using namespace std;
 
 int main() {
     int n;
-    cout<<"Nhap mang:";
+    cout<<"Nhap n:";
     cin>>n;
     int a[100];
     for(int i=0; i<n; i++){
-        cin>>a[i];
+        cin >> a[i];
     }
+    cout << "Mang vua nhap la:";
     for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(a[i]<a[j]){
-                int x= a[i];
-                a[i]= a[j];
-                a[j]= x;
-            }
+    for(int j=i+1; j<n; j++){
+        if(a[i]<a[j]){
+        int x= a[i];
+        a[i]= a[j];
+        a[j]= x;
+        }
         }
     }
     for(int i=0; i<n; i++){
-        cout<<a[i]<<"";
+    cout<<a[i]<<" ";
     }
-    return 0;
+return 0;
 }
